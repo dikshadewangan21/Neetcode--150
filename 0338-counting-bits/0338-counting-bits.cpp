442f863916c6536d699a722e8d464d06f444c721
Diksha Dewangan <dewangandiksha386@gmail.com>
2026-09-22 23:49:53 +0530

    Time: 0 ms (100%), Space: 10.5 MB (79.76%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+         vector<int> output(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            output[i] = output[i >> 1] + (i & 1);
+        }
+
+        return output;
+    }
+};
