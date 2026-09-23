47e93887eede7e5949458f6a428af8d211189a8a
Diksha Dewangan <dewangandiksha386@gmail.com>
2026-09-23 22:59:49 +0530

    Time: 4 ms (28.66%), Space: 8.1 MB (72.99%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            // Take the last bit of n
+            int bit = n & 1;
+
+            // Make space in result and add the bit
+            result = (result << 1) | bit;
+
+            // Move to the next bit
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
